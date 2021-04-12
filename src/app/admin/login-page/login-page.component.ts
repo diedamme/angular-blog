9b0dc3f4af08d0ef8ac7f3adb972bdd60b2ca714
SergeyMotailenko <MotailenkoSU@tomskasu.ru>
2021-04-12 10:31:59 +0700

    Созданы структура admin layout, страница входа, сервис для авторизации

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { ThrowStmt } from "@angular/compiler";
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { User } from "src/app/shared/interfaces";
+import { AuthService } from "../shared/services/auth.service";
 
 @Component({
-  selector: 'app-login-page',
-  templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  selector: "app-login-page",
+  templateUrl: "./login-page.component.html",
+  styleUrls: ["./login-page.component.scss"],
 })
 export class LoginPageComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private auth: AuthService, private router: Router) {}
 
   ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
   }
 
+  public submit(): void {
+    if (this.form.invalid) {
+      return;
+    }
+
+    const user: User = {
+      email: this.form.value.email,
+      password: this.form.value.password,
+    };
+
+    this.auth.login(user).subscribe(() => {
+      this.form.reset();
+      this.router.navigate(["/admin", "dashboard"]);
+    });
+  }
 }
