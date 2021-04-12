e50bb60f65ce87c632079770ad4510df4035c182
SergeyMotailenko <MotailenkoSU@tomskasu.ru>
2021-04-12 17:14:26 +0700

    Настройка firebase, хранение токена, обработка ошибок входа, настройка защиты роутов

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import { Enviroment } from "./interface";
+
+export const environment: Enviroment = {
+  production: true,
+  apiKey: "AIzaSyDXKTodNQXw3SzgMKUWgrhHPwNdG_QTEJM",
 };
