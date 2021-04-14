fec9eb751683cab1f7b8320856f5ae1a9cd0a92f
SergeyMotailenko <MotailenkoSU@tomskasu.ru>
2021-04-14 13:51:53 +0700

    Добавление формы создания поста

--- src/app/admin/create-page/create-page.component.ts
+++ src/app/admin/create-page/create-page.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { Post } from "src/app/shared/interfaces";
 
 @Component({
-  selector: 'app-create-page',
-  templateUrl: './create-page.component.html',
-  styleUrls: ['./create-page.component.scss']
+  selector: "app-create-page",
+  templateUrl: "./create-page.component.html",
+  styleUrls: ["./create-page.component.scss"],
 })
 export class CreatePageComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  constructor() {}
 
   ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl(null, Validators.required),
+      text: new FormControl(null, Validators.required),
+      author: new FormControl(null, Validators.required),
+    });
   }
 
+  submit() {
+    if (this.form.invalid) {
+      return;
+    }
+    const post: Post = {};
+  }
 }
--- src/app/shared/interfaces.ts
+++ src/app/shared/interfaces.ts
@@ -2,3 +2,11 @@ export interface User {
   email: string;
   password: string;
 }
+
+export interface Post {
+  id?: string;
+  title: string;
+  text: string;
+  author: string;
+  date: Date;
+}
